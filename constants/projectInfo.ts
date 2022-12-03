17869ee6bda8d02cbacf30d42401ac4b2ebdfcd2
Maxell Milay <milaymaxell@gmail.com>
2022-12-03 14:40:45 +0800

    updated medata site link

--- constants/projectInfo.ts
+++ constants/projectInfo.ts
@@ -14,7 +14,7 @@ const projectList = [
     description:
       'Medata is a media progress tracking platform, where users can track the progress of the media they are currently consuming (e.g. TV Series, Books, Anime). Users can add and track the media title, total content to consume (e.g. book pages, chapters, episodes), current content consumed, and others.',
     photoURL: medataImage,
-    link: 'https://medata-frontend-msjd-3t5a8r774-maxellmilay.vercel.app',
+    link: 'https://medata-frontend.vercel.app',
   },
 ];
 
